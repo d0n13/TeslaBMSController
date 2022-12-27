092520565a90a733091507544c0b831f6cef4495
Donie Kelly <donie.kelly@gmail.com>
2022-12-27 22:03:38 +0000

    Simplified loop

--- TeslaBMSController/src/main.cpp
+++ TeslaBMSController/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include "BMSManager.hpp"
-
 #include "display.hpp"
+
+#define SCREENTIME 6000
 
 BMSManager packManager = BMSManager();
 
@@ -10,23 +11,26 @@ void setup() {
   Serial.begin(115200);
 
   initDisplay();
+  packManager.findBoards();
 
-  Serial.printf("Internal Total heap %d, internal Free Heap %d\n", ESP.getHeapSize(), ESP.getFreeHeap());
-  Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
-  Serial.printf("ChipRevision %d, Cpu Freq %d, SDK Version %s\n", ESP.getChipRevision(), ESP.getCpuFreqMHz(), ESP.getSdkVersion());
-  Serial.printf("Flash Size %d, Flash Speed %d\n", ESP.getFlashChipSize(), ESP.getFlashChipSpeed());
+  // Serial.printf("Internal Total heap %d, internal Free Heap %d\n", ESP.getHeapSize(), ESP.getFreeHeap());
+  // Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+  // Serial.printf("ChipRevision %d, Cpu Freq %d, SDK Version %s\n", ESP.getChipRevision(), ESP.getCpuFreqMHz(), ESP.getSdkVersion());
+  // Serial.printf("Flash Size %d, Flash Speed %d\n", ESP.getFlashChipSize(), ESP.getFlashChipSpeed());
 }
 
 void loop() {
 
-    displayText(5, 5, 2, "Tesla BMS", SSD1327_WHITE);
-    displayText(5, 22, 1, "Operating Status:", 0x04);
+    while(true) {
 
-    Serial.println("Get BMS status");
+      packManager.modules[0].readModule();
 
-    packManager.findBoards();
-    BMSModule module = BMSModule(1);
-    module.readStatus();
-    module.readModuleValues();
-    delay(5000);
+      showPackStatusScreen(packManager);
+
+      delay(SCREENTIME);
+
+      showVoltageScreen(packManager);
+
+      delay(SCREENTIME);
+    }
 }
